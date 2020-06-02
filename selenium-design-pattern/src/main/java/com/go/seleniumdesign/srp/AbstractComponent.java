376cfb72307ef488ab8b8824b6098dcd41b871da
igorferro <igor.ferro.parreira@gmail.com>
2020-06-02 11:34:11 -0300

    Refactoring

--- selenium-design-pattern/src/main/java/com/go/seleniumdesign/srp/AbstractComponent.java
+++ selenium-design-pattern/src/main/java/com/go/seleniumdesign/srp/AbstractComponent.java
@@ -1,6 +1,19 @@
 package com.go.seleniumdesign.srp;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.*;
+
 public abstract class AbstractComponent {
+	
+	protected WebDriverWait wait;
+	
+	public AbstractComponent(final WebDriver driver) {
+		this.wait = new WebDriverWait(driver, 30);
+		PageFactory.initElements(driver, this);
+	}
+	
+	
 	public abstract boolean isDisplayed();
 
 }
--- selenium-design-pattern/src/main/java/com/go/seleniumdesign/srp/SearchWidget.java
+++ selenium-design-pattern/src/main/java/com/go/seleniumdesign/srp/SearchWidget.java
@@ -3,22 +3,16 @@ package com.go.seleniumdesign.srp;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.*;
 
 public class SearchWidget extends AbstractComponent {
 	
-	private WebDriverWait wait;
 	
 	@FindBy(name = "q")
 	private WebElement searchBox;
 	
 	
 	public SearchWidget(final WebDriver driver) {
-		this.wait = new WebDriverWait(driver,30);
-		PageFactory.initElements(driver, this);
+		super(driver);
 	}
 	
 	public void enter(String keyword) {
@@ -29,8 +23,7 @@ public class SearchWidget extends AbstractComponent {
 
 	@Override
 	public boolean isDisplayed() {
-		this.wait.until(ExpectedConditions.visibilityOf(this.searchBox));
-		return this.searchBox.isDisplayed();
+		return this.wait.until((d) -> this.searchBox.isDisplayed());
 	}
 
 }
