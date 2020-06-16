dcda6236535b779d69db15353c534a339c0193af
igorferro <igor.ferro.parreira@gmail.com>
2020-06-16 14:22:01 -0300

    Google Factory Implementation

---
+++ selenium-design-pattern/src/main/java/com/go/seleniumdesign/factory/GoogleArabic.java
@@ -0,0 +1,39 @@
+package com.go.seleniumdesign.factory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class GoogleArabic extends GoogleEnglish {
+	
+	
+	@FindBy(css = "div#gws-output-pages-elements-homepage_additional_languages__als a")
+	private WebElement language;
+	
+	@FindBy(css = "span.hOolge")
+	private WebElement keyboardBtn;
+	
+	@FindBy(id = "kbd")
+	private WebElement keyboard;
+	
+
+	public GoogleArabic(WebDriver driver) {
+		super(driver);
+	}
+	
+	@Override
+	public void launchSite() {
+		this.driver.get("https://www.google.sa");
+		this.language.click();
+	
+	}
+	
+	@Override
+	public void search(String keyword) {
+		this.wait.until((d) -> this.keyboardBtn.isDisplayed());
+		this.keyboardBtn.click();
+		this.wait.until((d) -> this.keyboard.isDisplayed());
+		super.search(keyword);
+	}
+
+}
