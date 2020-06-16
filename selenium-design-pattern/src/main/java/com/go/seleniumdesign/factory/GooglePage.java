2b81ccc385d28bc73d707659b17c38be0104b081
igorferro <igor.ferro.parreira@gmail.com>
2020-06-16 11:23:21 -0300

    Factory - Abstract Google Page

---
+++ selenium-design-pattern/src/main/java/com/go/seleniumdesign/factory/GooglePage.java
@@ -0,0 +1,9 @@
+package com.go.seleniumdesign.factory;
+
+public abstract class GooglePage {
+	
+	public abstract void launchSite();
+	public abstract void search(String keyword);
+	public abstract int getResultsCount();
+
+}
