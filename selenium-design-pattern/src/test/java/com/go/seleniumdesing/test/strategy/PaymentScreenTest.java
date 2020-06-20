55b9f0c42b929bc826b9e72870245e9b0e0297a1
igorferro <igor.ferro.parreira@gmail.com>
2020-06-20 06:54:34 -0300

    Strategy - Payment Screen Test - Part 2

--- selenium-design-pattern/src/test/java/com/go/seleniumdesing/test/strategy/PaymentScreenTest.java
+++ selenium-design-pattern/src/test/java/com/go/seleniumdesing/test/strategy/PaymentScreenTest.java
@@ -3,8 +3,12 @@ package com.go.seleniumdesing.test.strategy;
 import java.util.Map;
 
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.collections.Maps;
 
+import com.go.seleniumdesign.strategy.CreditCard;
+import com.go.seleniumdesign.strategy.NetBanking;
 import com.go.seleniumdesign.strategy.PaymentOption;
 import com.go.seleniumdesign.strategy.PaymentScreen;
 import com.go.seleniumdesing.test.BaseTest;
@@ -19,7 +23,7 @@ public class PaymentScreenTest extends BaseTest{
 		
 	}
 	
-	@Test
+	@Test(dataProvider = "getData")
 	public void paymentTest(PaymentOption paymentOption, Map<String, String> paymentDetails) {
 		this.paymentScreen.goTo();
 		this.paymentScreen.getUserInformantion().enterDetails();
@@ -31,5 +35,26 @@ public class PaymentScreenTest extends BaseTest{
 		System.out.println("Order Number : " +orderNumber);
 		
 	}
-
+	
+	@DataProvider
+	public Object[][] getData(){
+		
+		Map<String, String> cc = Maps.newHashMap();
+		cc.put("cc", "123434324");
+		cc.put("year", "2026");
+		cc.put("cvv", "123");
+		
+		Map<String, String> nb = Maps.newHashMap();
+		nb.put("bank", "WELLS FARGO");
+		nb.put("acount", "myaccount123");
+		nb.put("pin", "123");
+		
+		
+		return new Object[][] {
+			{new CreditCard(), cc},
+			{new NetBanking(), nb}
+		};
+		
+	}
+	
 }
