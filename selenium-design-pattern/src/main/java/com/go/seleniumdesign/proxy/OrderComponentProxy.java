39716c78eadb1ab2dadfd818c4d3db285888e435
igorferro <igor.ferro.parreira@gmail.com>
2020-06-23 06:34:03 -0300

    Proxy - Order Component Proxy - Implementation

--- selenium-design-pattern/src/main/java/com/go/seleniumdesign/proxy/OrderComponentProxy.java
+++ selenium-design-pattern/src/main/java/com/go/seleniumdesign/proxy/OrderComponentProxy.java
@@ -1,5 +1,30 @@
 package com.go.seleniumdesign.proxy;
 
-public class OrderComponentProxy {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+import org.openqa.selenium.WebDriver;
+
+public class OrderComponentProxy implements OrderComponent{
+	
+	public static final List<String>EXCLUDED = Arrays.asList("PROD", "STAGING");
+	private OrderComponent orderComponent;
+	
+	public OrderComponentProxy(WebDriver driver) {
+		String currentEnv = System.getProperty("env"); //DEV ,QA, PROD, STAGING
+		if(!EXCLUDED.contains(currentEnv)) {
+			this.orderComponent = new OrderComponentReal(driver);
+		}
+	}
+
+	@Override
+	public String placeOrder() {
+		if(Objects.nonNull(this.orderComponent)) {
+			return this.orderComponent.placeOrder();
+		}else {
+			return "SKIPPED";
+		}
+	}
 
 }
